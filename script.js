39240dd8887d9ef47f34ec161b8e179822fce5a3
Jan_Jagoda <jan.jagoda@outlook.com>
2023-08-14 21:30:58 +0200

    Exercise 1 completed

---
+++ script.js
@@ -0,0 +1,10 @@
+// Javascript Objects
+
+// Exercise 1
+
+function isObject(valueOne) {
+    return Boolean(valueOne);
+}
+
+console.log(isObject({})); // true
+console.log(isObject(null)); // false
