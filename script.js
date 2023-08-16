a0bb0d3e68c9c125299d8f67a1c3e3f323466c8d
Jan_Jagoda <jan.jagoda@outlook.com>
2023-08-16 21:54:00 +0200

    Completed Exercise 9

--- script.js
+++ script.js
@@ -108,3 +108,22 @@ function animalSentence(animalToSentence) {
     return `This ${animalToSentence.color} ${animalToSentence.name} has ${animalToSentence.legs} legs.`
 }
 console.log(animalSentence(animal));
+
+// Exercise 9
+
+const rooms = {
+    first: {
+        description: 'This is the first room',
+        items: {
+            chair: 'The old chair looks comfortable',
+            lamp: 'This lamp looks ancient'
+        }
+    },
+    second: {
+        description: 'This is the second room',
+        items: {
+            couch: 'This couch looks like it would hurt your back',
+            table: 'On the table there is an unopened bottle of water'
+        }
+    }
+}
