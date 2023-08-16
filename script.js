8f5b34afd3c3842633fef2399e399cdee66ecfbd
Jan_Jagoda <jan.jagoda@outlook.com>
2023-08-16 18:39:11 +0200

    Completed Exercise 2

--- script.js
+++ script.js
@@ -2,9 +2,28 @@
 
 // Exercise 1
 
-function isObject(valueOne) {
-    return Boolean(valueOne);
+function isObject(value) {
+    return typeof value === "object" && value !== null;
 }
 
 console.log(isObject({})); // true
 console.log(isObject(null)); // false
+
+// Exercise 2
+
+const user = {
+    name: 'John'
+}
+
+function removeProperty(propertyObject, propertyKey) {
+    if (propertyObject[propertyKey] !== undefined) {
+        return delete propertyObject[propertyKey];
+    }
+    return false
+}
+console.log(removeProperty(user, 'name')); // true
+console.log(removeProperty(user, 'name')); // false
+console.log(removeProperty(user, 'weight')); // false
+
+console.log(user);
+
