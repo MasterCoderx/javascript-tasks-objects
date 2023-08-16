34925b18e7e0069622ca72b16b92803c7cf2d262
Jan_Jagoda <jan.jagoda@outlook.com>
2023-08-16 19:19:21 +0200

    Completed Exercise 3

--- script.js
+++ script.js
@@ -27,3 +27,23 @@ console.log(removeProperty(user, 'weight')); // false
 
 console.log(user);
 
+// Exercise 3
+
+const firstJohn = {
+    firstName: 'John',
+    lastName: 'Smith'
+}
+const secondJohn = {
+    firstName: 'John',
+    lastName: 'Smith'
+}
+const adam = {
+    firstName: 'Adam',
+    lastName: 'Johnson'
+}
+
+function checkIfUsersHaveTheSameName(userOne, userTwo) {
+    return (userOne['firstName'] === userTwo['firstName']) && (userOne['lastName'] === userTwo['lastName']);
+}
+console.log(checkIfUsersHaveTheSameName(firstJohn, secondJohn)); // true
+console.log(checkIfUsersHaveTheSameName(firstJohn, adam)); // false
